c0fa5eb7461572dc78bade4f4607f58a7be236e1
NitiwatOwen <nitiwat3478@gmail.com>
2023-10-22 13:27:02 +0700

    init prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookingModule } from './booking/booking.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [BookingModule],
+  imports: [BookingModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
