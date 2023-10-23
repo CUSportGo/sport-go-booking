b887985f5d139bebbaaeb9424f3ae847e9b629f7
pawarisble <blebikebe@gmail.com>
2023-10-23 14:29:05 +0700

    add booking proto and GetAvailableBooking

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -1,15 +1,23 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+import { GrpcMethod, MessagePattern } from '@nestjs/microservices';
 import { CREATE_BOOKING_PATTERN } from '../constant/booking.constant';
 import { BookingInfo } from './booking.dto';
 import { BookingService } from './booking.service';
+import { GetAvailableBookingRequest, GetAvailableBookingResponse } from './booking.pb';
+
+
 
 @Controller('booking')
 export class BookingController {
-  constructor(private bookingService: BookingService) {}
+  constructor(private bookingService: BookingService) { }
 
   @MessagePattern(CREATE_BOOKING_PATTERN)
   public async createBooking(booking: BookingInfo) {
     return this.bookingService.createBooking(booking);
+  }
+
+  @GrpcMethod("BookingService", "GetAvailableBooking")
+  getAvailableBooking(request: GetAvailableBookingRequest): Promise<GetAvailableBookingResponse> {
+    return this.bookingService.GetAvailableBooking(request);
   }
 }
--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -9,6 +9,9 @@ import { BookingInfo } from './booking.dto';
 import { v4 as uuidv4 } from 'uuid';
 import { SportareaService } from '../sportarea/sportarea.service';
 import { commonUtils } from '../utils/common.utils';
+import { GetAvailableBookingRequest, GetAvailableBookingResponse, TimeSlot } from './booking.pb';
+
+
 
 @Injectable()
 export class BookingService {
@@ -60,6 +63,79 @@ export class BookingService {
       const newBooking = await this.bookingRepo.create(createdBooking);
       console.log(newBooking);
       // notify back to user
+    } catch (error) {
+      console.log(error);
+      if (!(error instanceof HttpException)) {
+        throw new InternalServerErrorException('Internal server error');
+      }
+      throw error;
+    }
+  }
+
+  async createAvailableTimeSlots(
+    startTime: Date,
+    endTime: Date,
+    timeIntervalInMinutes: number
+  ): Promise<string[]> {
+    try {
+      const timeSlots: string[] = [];
+      let currentTime = new Date(startTime);
+
+      while (currentTime < endTime) {
+        const slotStartTime = currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        currentTime.setMinutes(currentTime.getMinutes() + timeIntervalInMinutes);
+        const slotEndTime = currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+        timeSlots.push(`${slotStartTime}-${slotEndTime}`);
+      }
+      console.log(timeSlots);
+      return timeSlots;
+    } catch (error) {
+      console.log(error);
+      if (!(error instanceof HttpException)) {
+        throw new InternalServerErrorException('Internal server error');
+      }
+      throw error;
+    }
+  }
+
+  convertTimeToProto = (timeList: string[]): TimeSlot[] => {
+    return timeList.map(timeSlot => {
+      const [startTime, endTime] = timeSlot.split('-');
+      return {
+        startTime,
+        endTime,
+      };
+    });
+  }
+
+  async GetAvailableBooking(request: GetAvailableBookingRequest): Promise<GetAvailableBookingResponse> {
+    try {
+      const area = await this.sportareaService.getAreaById({
+        sportAreaId: request.sportAreaId,
+        sportType: request.sportType,
+        areaId: request.areaId,
+      });
+
+      const allTimeSlot = await this.createAvailableTimeSlots(new Date(area.data.openTime), new Date(area.data.closeTime), 60)
+      const listTime: string[] = [];
+
+      for (const timeSlot of allTimeSlot) {
+        const [startTime, endTime] = timeSlot.split('-');
+        const bookingList = await this.bookingRepo.checkAvailability(
+          request.sportAreaId,
+          request.sportType,
+          request.areaId,
+          startTime,
+          endTime
+        );
+        if (bookingList.length === 0) {
+          listTime.push(timeSlot);
+        }
+      }
+      const listAvailableTime: TimeSlot[] = this.convertTimeToProto(listTime);
+      return { listAvailableTime };
+
     } catch (error) {
       console.log(error);
       if (!(error instanceof HttpException)) {
