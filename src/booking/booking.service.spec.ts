431b99b91c6d8c133b3c704a915dbb2b19c77c7a
NitiwatOwen <nitiwat3478@gmail.com>
2023-11-09 20:27:23 +0700

    fix failed test

--- src/booking/booking.service.spec.ts
+++ src/booking/booking.service.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { BookingRepository } from '../repository/booking.repository';
+import { SportareaService } from '../sportarea/sportarea.service';
+import { UserService } from '../user/user.service';
 import { BookingService } from './booking.service';
 
 describe('BookingService', () => {
   let service: BookingService;
 
+  const mockBookingRepo = {};
+  const mockSportAreaService = {};
+  const mockUserService = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BookingService],
+      providers: [
+        BookingService,
+        {
+          provide: BookingRepository,
+          useValue: mockBookingRepo,
+        },
+        {
+          provide: SportareaService,
+          useValue: mockSportAreaService,
+        },
+        {
+          provide: UserService,
+          useValue: mockUserService,
+        },
+      ],
     }).compile();
 
     service = module.get<BookingService>(BookingService);
--- src/sportarea/sportarea.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { SportareaService } from './sportarea.service';
-
-@Controller('sportarea')
-export class SportareaController {
-  constructor(private sportareaService: SportareaService) {}
-}
