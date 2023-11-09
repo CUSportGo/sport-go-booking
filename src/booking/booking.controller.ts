185bfcdf29d4268b05daec77e01cde648b1c9821
NitiwatOwen <nitiwat3478@gmail.com>
2023-11-09 20:33:42 +0700

    fix

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -1,16 +1,35 @@
 import { Controller } from '@nestjs/common';
-import { GrpcMethod, Ctx, MessagePattern, Payload, RmqContext } from '@nestjs/microservices';
-import { CANCEL_BOOKING_PATTERN, CONFIRM_BOOKING_PATTERN, CREATE_BOOKING_PATTERN } from '../constant/booking.constant';
-import { BookingInfo, CancelBookingInfo, ConfirmBookingInfo } from './booking.dto';
+import {
+  GrpcMethod,
+  Ctx,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
+import {
+  CANCEL_BOOKING_PATTERN,
+  CONFIRM_BOOKING_PATTERN,
+  CREATE_BOOKING_PATTERN,
+} from '../constant/booking.constant';
+import {
+  BookingInfo,
+  CancelBookingInfo,
+  ConfirmBookingInfo,
+} from './booking.dto';
 import { BookingService } from './booking.service';
-import { GetAvailableBookingRequest, GetAvailableBookingResponse } from './booking.pb';
-
-
-import { BookingServiceController, ViewBookingHistoryRequest, ViewBookingHistoryResponse } from './booking.pb';
+import {
+  GetAvailableBookingRequest,
+  GetAvailableBookingResponse,
+} from './booking.pb';
+import {
+  BookingServiceController,
+  ViewBookingHistoryRequest,
+  ViewBookingHistoryResponse,
+} from './booking.pb';
 
 @Controller('booking')
 export class BookingController implements BookingServiceController {
-  constructor(private bookingService: BookingService) { }
+  constructor(private bookingService: BookingService) {}
 
   @MessagePattern(CREATE_BOOKING_PATTERN)
   public async createBooking(booking: BookingInfo) {
@@ -18,8 +37,10 @@ export class BookingController implements BookingServiceController {
   }
 
   @GrpcMethod('BookingService', 'ViewBookingHistory')
-  public async viewBookingHistory(request: ViewBookingHistoryRequest): Promise<ViewBookingHistoryResponse> {
-    return await this.bookingService.viewBookingHistory(request)
+  public async viewBookingHistory(
+    request: ViewBookingHistoryRequest,
+  ): Promise<ViewBookingHistoryResponse> {
+    return await this.bookingService.viewBookingHistory(request);
   }
 
   @MessagePattern(CANCEL_BOOKING_PATTERN)
@@ -32,8 +53,10 @@ export class BookingController implements BookingServiceController {
     return this.bookingService.confirmBooking(confirmInfo);
   }
 
-  @GrpcMethod("BookingService", "GetAvailableBooking")
-  getAvailableBooking(request: GetAvailableBookingRequest): Promise<GetAvailableBookingResponse> {
+  @GrpcMethod('BookingService', 'GetAvailableBooking')
+  getAvailableBooking(
+    request: GetAvailableBookingRequest,
+  ): Promise<GetAvailableBookingResponse> {
     return this.bookingService.GetAvailableBooking(request);
   }
 }
